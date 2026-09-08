b0758bdbf2cf76451443ff9189412f51c6d9aca7
mujib1806 <mujiburrohmanahmad@gmail.com>
2026-09-08 20:36:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -316,7 +316,7 @@ function bukaLayarAplikasi() {
     document.getElementById('menuMutasi').style.display = isOwner ? 'block' : 'none';        
     document.getElementById('menuGaji').style.display = isOwner ? 'block' : 'none';      
     // KODE BARU: Memunculkan menu Dashboard Global khusus Owner
-    cconst menuGlobal = document.getElementById('menuDashboardGlobal');
+    const menuGlobal = document.getElementById('menuDashboardGlobal');
     if (menuGlobal) menuGlobal.style.display = isOwner ? 'block' : 'none';
     document.getElementById('menuDashboard').style.display = (isOwner || isDapur) ? 'block' : 'none';        
     document.getElementById('menuProduk').style.display = isOwner ? 'block' : 'none';        
